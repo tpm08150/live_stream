1692127b5e3a78e85dd2c0543858b76a0cff2897
tpm08150 <63214782+tpm08150@users.noreply.github.com>
2022-03-14 20:42:00 -0500

    Update livestream.py

--- livestream.py
+++ livestream.py
@@ -22,6 +22,8 @@ format_code = "Hi59"
 
 stream = f'ffmpeg -y -format_code {format_code} -f decklink -i "{path}" -c:v libx264 -crf 20.0 -crf_max 25.0 -profile high -preset veryfast -trellis 2 -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv {rtmp}/{key}'
 
+#stream = f'ffmpeg -re -stream_loop -1 -i {path} -map 0 -flags +global_header -c:v libx264 -crf 20.0 -crf_max 25.0 -profile high444 -preset veryfast -trellis 2 -maxrate 2600k -bufsize 5200k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f tee "[f=flv]{rtmp}/{key}|[f=flv]{stream_name}.mp4"'
+
 # stream = f'ffmpeg -y -thread_queue_size 90 -hwaccel cuda -channels 2 -raw_format yuv422p10 -format_code {format_code} -f decklink -i "{path}" -c:a pcm_s16le -c:v libx264 -crf 20.0 -crf_max 25.0 -preset fast -trellis 2 -maxrate 2600k -bufsize 5200k -f flv {rtmp3}{key3}'
 
 # stream = f'ffmpeg -format_code Hi60 -f decklink -i "DeckLink SDI 4K" -c:a copy -c:v copy output.avi'
